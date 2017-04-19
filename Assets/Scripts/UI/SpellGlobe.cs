748adbf04dc34d726dfe08a694c8117454c2c722
exdunn <dkvcio2n!>
2017-04-19 00:41:38 -0700

    add health globe sprite add spell icon sprite

---
+++ Assets/Scripts/UI/SpellGlobe.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WizardWars
+{
+    public class SpellGlobe : MonoBehaviour {
+
+
+        #region Public Variables
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary>
+        /// Spell icon image
+        /// </summary>
+        private GameObject icon;
+
+        /// <summary>
+        /// Radial fill image that is set to 1 when player casts the spell then
+        /// contracts to 0 as as cooldownTimer increases
+        /// </summary>
+        private GameObject fill;
+
+        /// <summary>
+        /// Timer that starts when a player uses the spell and resets when cooldownTimer equals the spell's cooldown
+        /// </summary>
+        private float cooldownTimer;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        // Use this for initialization
+        void Start () {
+
+            icon = transform.GetChild(0).gameObject;
+            fill = transform.GetChild(1).gameObject;
+
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+
+            // set fill to display remaining cooldown
+            fill.GetComponent<Image>().fillAmount = cooldownTimer;
+
+
+	    }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Set icon's image to the designated sprite
+        /// </summary>
+        /// <param name="sprite"></param>
+        public void SetIcon(Sprite sprite)
+        {
+            icon.GetComponent<Image>().sprite = sprite;
+        }
+
+        #endregion
+
+    }
+
+}
