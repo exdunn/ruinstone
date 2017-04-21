6bed864bd1009f6e0967b43f43767bfd6d16d73e
exdunn <dkvcio2n!>
2017-04-21 11:14:54 -0700

    add collection page add rune sprites

---
+++ Assets/Scripts/UI/RuneUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace WizardWars
+{
+    public class RuneUI : MonoBehaviour
+        , IPointerEnterHandler
+        , IPointerExitHandler
+    {
+        #region Public Variables
+
+        public Sprite normal;
+        public Sprite highlighted;
+        public GameObject runeImage;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            runeImage.GetComponent<Image>().sprite = highlighted;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            runeImage.GetComponent<Image>().sprite = normal;
+        }
+
+        #endregion
+
+    }
+}
+
