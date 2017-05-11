56f20546905dc89659f2bb385bcd79ccb4fbb5e0
exdunn <ahdunn@uci.edu>
2017-05-11 16:42:40 -0700

    add quark spell

--- Assets/Scripts/SpellsSystem/Spells2.0/SpellLibrary/Spell/S006_Quark.cs
+++ Assets/Scripts/SpellsSystem/Spells2.0/SpellLibrary/Spell/S006_Quark.cs
@@ -18,7 +18,7 @@ namespace SpellSystem {
 
             Vector3 spawnPos = new Vector3(caster.transform.position.x, caster.transform.position.y + 1, caster.transform.position.z);
 
-            GameObject proj = SpellUtility.SpawnProjectile("Spells/" + spawn, this.transform, spawnPos, Quaternion.identity);
+            GameObject proj = SpellUtility.SpawnProjectile("Spells/" + spawn, this.transform, spawnPos, Quaternion.identity, _stats.GetRadius());
             proj.GetComponent<Projectile>()._stats = _stats;
             proj.GetComponent<Projectile>().Move(caster, point);
             StartCoroutine(Cooldown());
