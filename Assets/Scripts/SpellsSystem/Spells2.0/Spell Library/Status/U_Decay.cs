8a5c769e72c21373966359f9013a884a14b9972a
Joseph <josephpkc@gmail.com>
2017-05-20 15:56:28 -0700

    Entropic Decay, Decay status

---
+++ Assets/Scripts/SpellsSystem/Spells2.0/Spell Library/Status/U_Decay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WizardWars;
+namespace SpellSystem {
+    public class U_Decay : Status {
+        public float _damageReceivedModifier;
+        public float _slowModifier;
+        public override void Activate(GameObject target, int where) {
+            if(isStarting) {
+                return;
+            }
+            isStarting = true;
+            _timer = 0f;
+            this.target = target;
+            this.where = where;
+            PlayerManager player = target.GetComponent<PlayerManager>();
+            if(!player) {
+                return;
+            }
+
+            //player.GetComponent<PhotonView>().RPC("UpdateDamageModifier", PhotonTargets.All, player.damageModifier + _damageModifier);
+            player.GetComponent<PhotonView>().RPC("UpdateDamageReceivedModifier", PhotonTargets.All, player.damageReceivedModifier + _damageReceivedModifier);
+
+            StartCoroutine(Run());
+        }
+
+        public override void Deactivate(GameObject target) {
+            PlayerManager player = target.GetComponent<PlayerManager>();
+            if(!player) {
+                return;
+            }
+            //player.GetComponent<PhotonView>().RPC("UpdateDamageModifier", PhotonTargets.All, player.damageModifier + _damageModifier);
+            player.GetComponent<PhotonView>().RPC("UpdateDamageReceivedModifier", PhotonTargets.All, player.damageReceivedModifier + _damageReceivedModifier);
+        }
+
+        protected override IEnumerator Run() {
+            while(_timer < _duration) {
+                yield return new WaitForSeconds(SpellUtility.TICK);
+                _timer += SpellUtility.TICK;
+            }
+            isEnd = true;
+        }
+
+    }
+}
+
